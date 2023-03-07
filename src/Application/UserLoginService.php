3462210a4e5225bb9c0c0300e569b2c99bf4d9df
Iván Sola Delgado <ivan@MacBook-Air-de-Ivan.local>
2023-03-07 17:54:36 +0100

    First commit, step 2 to do with stub and dummy

--- src/Application/UserLoginService.php
+++ src/Application/UserLoginService.php
@@ -2,13 +2,33 @@
 
 namespace UserLoginService\Application;
 
+use Exception;
+use UserLoginService\Domain\User;
+use UserLoginService\Infrastructure\FacebookSessionManager;
+
 class UserLoginService
 {
-    private array $loggedUsers = [];
+    private array $loggedUsers = ["username"];
 
-    public function manualLogin(): string
+    /**
+     * @throws Exception
+     */
+    public function manualLogin(User $user): void
     {
-        return "user logged";
+        if(in_array($user,$this->loggedUsers)){
+            throw new Exception('User already logged in');
+        }
+       $this->loggedUsers[] = $user;
     }
 
+    public function getLoggedUsers(): array
+    {
+        return $this->loggedUsers;
+    }
+
+    public function getExternalSessions(): int
+    {
+        $numberOfActiveSessions = new FacebookSessionManager();
+        return $numberOfActiveSessions->getSessions();
+    }
 }
--- tests/Application/UserLoginServiceTest.php
+++ tests/Application/UserLoginServiceTest.php
@@ -4,18 +4,60 @@ declare(strict_types=1);
 
 namespace UserLoginService\Tests\Application;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
 use UserLoginService\Application\UserLoginService;
+use UserLoginService\Domain\User;
 
 final class UserLoginServiceTest extends TestCase
 {
     /**
      * @test
      */
-    public function userIsLoggedIn()
+
+    public function errorWhileManuallyLoginUserIfIsAlreadyLogged()
     {
         $userLoginService = new UserLoginService();
+        $user = new User("username");
 
-        $this->assertEquals("user logged", $userLoginService->manualLogin());
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('User already logged in');
+
+        $userLoginService->manualLogin($user);
+        $userLoginService->manualLogin($user);
     }
+
+    /**
+     * @test
+     * @throws Exception
+     */
+
+    public function userIsManuallyLoggedIn()
+    {
+        $userLoginService = new UserLoginService();
+        $user = new User("name");
+
+        $userLoginService->manualLogin($user);
+
+        $this->assertContains($user,$userLoginService->getLoggedUsers());
+    }
+
+    /**
+     * @test
+     */
+
+    public function returnTheNumberOfActiveSessions()
+    {
+        $userLoginService = new UserLoginService();
+        $user = new User("name");
+
+        $userLoginService->manualLogin($user);
+
+        $this->assertEquals(1,$userLoginService->getExternalSessions()); //Realizar un stub y un dummy
+        /*
+         * Como crear stub, en vez de crear un session manager crearemos un stubFacebookSessionManager.php, con los mismos metodos
+         * necesitaremos un stub y un dummy para la sessionManager
+         */
+    }
+
 }
